5ad37ca346cdc797d2d18d44b926635826015d3e
Mayu Laierlence <mayuki.minano@gmail.com>
2013-09-06 00:43:37 +0900

    support new web

--- autopoke.js
+++ autopoke.js
@@ -1,1 +1,1 @@
-setInterval(function(){var _=document.body.querySelectorAll('#pagelet_pokes li a.uiIconText'),a=0;for(;a<_.length;a++)_[a].click()},2000)
+setInterval(function(){var _=document.body.querySelectorAll('#pagelet_pokes [ajaxify*="/poke_inline.php"]'),a=0;for(;a<_.length;a++)_[a].click()},2000)
